38943f37b2afae5efe1630b05f7c41f2f26e6eef
pavel <shirokuiuna@mail.ru>
2026-03-21 12:35:32 +0300

    Базованя навигация

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,22 +1,5 @@
-import { Helmet, HelmetProvider } from 'react-helmet-async';
-
-import CenterLayout from 'src/shared/layouts/CenterLayout/CenterLayout';
-import PageLayout from 'src/shared/layouts/PageLayout/PageLayout';
-
 function MainPage() {
-  return (
-    <HelmetProvider>
-      <PageLayout
-      >
-        <Helmet>
-          <title>Домашняя бухгалтерия</title>
-        </Helmet>
-        <CenterLayout>
-          <h1>Hello World</h1>
-        </CenterLayout>
-      </PageLayout>
-    </HelmetProvider>
-  );
+  return <h1>Hello World</h1>;
 }
 
 export default MainPage;
